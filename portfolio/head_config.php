9f4f64d6690d6ceb540a454a47bc9056ab7f38b4
comude <pkb5656@gmail.com>
2016-01-24 19:45:00 +0900

    test

---
+++ portfolio/head_config.php
@@ -0,0 +1,16 @@
+<!--부트스트랩-->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="./bootstrap-3.3.4-dist/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!--kb style.css-->
+<link href="kbstyle.css" rel="stylesheet">
+
+<!--Pure css-->
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+
+<!--Google font-->
+<link href='https://fonts.googleapis.com/css?family=Asap' rel='stylesheet' type='text/css'>
+
+<meta charset="UTF-8">
+<title>KB Portfolio</title>
