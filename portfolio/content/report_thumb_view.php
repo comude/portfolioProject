9ed4c601d5ff6864216281903fd5bae650797eba
comude <pkb5656@gmail.com>
2016-01-26 02:18:55 +0900

    write화면버그 수정

--- portfolio/content/report_thumb_view.php
+++ portfolio/content/report_thumb_view.php
@@ -20,52 +20,107 @@
 
 <!-------------------------------- low_body -------------------------------->
 
-<div class="row">
+    <div class="row">
 
-    <div class="col-md-offset-2 col-md-8 " id="low_body">
+        <div class="col-md-offset-2 col-md-8 " id="low_body">
 
-        <!--left_nav-->
-        <?php include "../left_nav.php"; ?>
+            <!--left_nav-->
+            <?php include "../left_nav.php"; ?>
 
-        <!--contents-->
+            <!--contents-->
 
-        <div class="col-md-8" style="overflow:scroll;">
+            <div class="col-md-8" style="overflow:scroll;">
 
-            <div id="title_container">
-                Projects
+                <div id="title_container">
+                    Projects
+                </div>
+
+    <!--          1. 여기 db에서 project들의 목록을 받아와서 뿌려주는 과정필요-->
+
+                    <?php
+                        require("../db_config.php");
+
+                        $sql = "SELECT * FROM thumbnail";
+                        $result = mysqli_query($conn,$sql);
+
+
+
+                        while($row = mysqli_fetch_assoc($result)){
+
+                            // img_path 작업
+                              $img_path = "../img/"."{$row['img_src']}";
+                    ?>
+                            <!--thumbnail-->
+                            <div id="thumb_container">
+                                <table>
+
+                                    <tr>
+                                        <td>
+                                            <a href="./report_view.php">
+                                                <input type="image" id="thumb_img" src="<?php echo $img_path; ?>")>
+                                            </a>
+                                        </td>
+
+                                        <td>
+                                            <form class="pure-form pure-form-aligned">
+
+                                                <div class="panel-default" id="thumb_description">
+                                                    <fieldset>
+                                                        <div class="pure-control-group">
+                                                            <label style="width:130px;">Project name :</label>
+                                                             <?php echo $row['proj_name']; ?>
+                                                        </div>
+
+                                                        <div class="pure-control-group">
+                                                            <label style="width:130px;">User name :</label>
+                                                            <?php echo $row['user_name']; ?>
+                                                        </div>
+
+                                                        <div class="pure-control-group">
+                                                            <label style="width:130px;">Section :</label>
+                                                            <?php echo $row['section']; ?>
+                                                        </div>
+
+                                                        <div class="pure-control-group">
+                                                            <label style="width:130px;">Period :</label>
+                                                            <?php echo $row['period']; ?>
+                                                        </div>
+                                                    </fieldset>
+                                                </div>
+                                            </form>
+
+                                        </td>
+                                    </tr>
+
+                                </table>
+
+                            </div>
+
+                     <?php  }?>
+
+
+
+    <!--          2. '새로운 글쓰기' 버튼-->
+
+                <form action="report_thumb_write.php">
+                    <input type="submit" value="write" >
+                </form>
+
             </div>
 
-<!--          1. 여기 db에서 project들의 목록을 받아와서 뿌려주는 과정필요-->
-<!--          2. '새로운 글쓰기' 버튼-->
-
-            <form action="report_thumb_write.php">
-                <input type="submit" value="write" >
-            </form>
-
-        </div>
 
 
 
 
+            <!--comment-->
+            <div class="col-md-2" id="comment">comment</div>
 
 
 
-
-        </div>
-
-        <!--comment-->
-        <div class="col-md-2" id="comment">comment</div>
+        </div> <!--end of low_body-->
 
 
-
-
-
-
-
-    </div>
-
-
-</div>
+    </div> <!--end of class="row"-->
 
 
 
--- portfolio/db_config.php
+++ portfolio/db_config.php
@@ -1,4 +1,4 @@
 <?php
-
     $conn = mysqli_connect("localhost","root","111111");
-    mysqli_select_db($conn,"portfolio");
+    mysqli_select_db($conn,'portfolio');
+?>
